285b0e37e8051297bca4bfaaf32eda289c3d978c
michieljoris <michieljoris@gmail.com>
2013-11-18 00:57:10 +1000

    sync

---
+++ sync-to-linode.sh
@@ -0,0 +1,10 @@
+rsync --verbose  --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude "*bak" --exclude "*~" --exclude ".git" ~/www/sites/roster_help/ michieljoris@linode:~/www/roster_help/
+
+
+
+
+
+
+
+
+
